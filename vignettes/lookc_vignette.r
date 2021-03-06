c14e6366345ca7ace5293d7128aa482fb4c03119
ekernf01 <eric.kern13@gmail.com>
2021-03-05 19:34:43 -0500

    Add speed tests to vignette

--- vignettes/lookc_vignette.r
+++ vignettes/lookc_vignette.r
@@ -10,24 +10,41 @@ for(k in 2:p){
   }
 }
 Sigma = cov(X)
+mu = rep(0, ncol(X))
 # Leave one out using Dr. Sesia's original code
 filter_100 = knockoff::knockoff.filter(X[,-100], y = X[,100], statistic = knockoff::stat.glmnet_coefdiff, fdr = 0.5)
 # Leave one out using a wrapper to Dr. Sesia's original code
-stats_from_wrapper      = rlookc::generateLooksSlow(X, mu = 0, Sigma = Sigma, vars_to_omit = 100, output_type = "statistics")
+stats_from_wrapper      = rlookc::generateLooksSlow(X, mu = mu, Sigma = Sigma, vars_to_omit = 100, output_type = "statistics")
 # Leave one out using a reimplementation that I hope will eventually be faster for our use case
-stats_from_reimplementation = rlookc::generateLooks(X, mu = 0, Sigma = Sigma, vars_to_omit = 100, output_type = "statistics")
+stats_from_reimplementation = rlookc::generateLooks(X, mu = mu, Sigma = Sigma, vars_to_omit = 100, output_type = "statistics")
 
-# All three show good error control on this toy example
+# All three show a very obvious correct signal on this toy example
 filter_100
 plot(stats_from_wrapper[[1]])
 plot(stats_from_reimplementation[[1]])
 
-# Regarding scaling, here's 10 looks by brute force and by low-rank updating.
+# Regarding scaling, here are 5 looks done by brute force and then by low-rank updating.
+# On my 2016 Dell XPS13 with Dual-Core Intel® Core™ i5-6200U CPU @ 2.30GHz,
+# the low-rank updates win by a factor of 4, 46s to 11s.
 microbenchmark::microbenchmark(
   times = 1,
-  looks_from_reimplementation = rlookc::generateLooks(X, mu = 0, Sigma = Sigma, vars_to_omit = 96:100, output_type = "knockoffs")
+  looks_brute_force =  lapply(96:100, function(k) {knockoff::create.gaussian(X[,-k], mu = mu[-k], Sigma = Sigma[-k,-k])})
 )
 microbenchmark::microbenchmark(
   times = 1,
-  looks_brute_force =  lapply(96:100, function(k) {knockoff::create.gaussian(X[,-k], mu = 0, Sigma = Sigma)})
+  looks_from_reimplementation = rlookc::generateLooks(X, mu = mu, Sigma = Sigma, vars_to_omit = 96:100, output_type = "knockoffs")
+)
+
+# In fact, we can we generate all of the LOOKs and compute statistics in 105s.
+do_pearson_screen = function(X, ko, y){
+  rbind(cor(X, y), cor(ko, y)) %>% set_rownames(c("X", "knockoff"))
+}
+microbenchmark::microbenchmark(
+  times = 1,
+  looks_from_reimplementation = rlookc::generateLooks(X,
+                                                      mu = mu,
+                                                      Sigma = Sigma,
+                                                      vars_to_omit = 1:500,
+                                                      statistic = do_pearson_screen,
+                                                      output_type = "statistic")
 )
