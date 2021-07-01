6112c3ee4c4120a9bc064fe80435c63d038b0191
ekernf01 <eric.kern13@gmail.com>
2021-07-01 13:47:26 -0400

    Add and test two new diagnostic features for more sensitive tests of breakdown of FDR control.

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,3 +1,63 @@
+#' Pick P(Y|X) to span a wide range of possibilities.
+#'
+#' @param X  Covariates from which you want to select an active set, but you are
+#' afraid your modeling assumptions might be wrong.
+#' @param n_quantiles Each variable is split at this many quantiles and
+#' Y is set to a collection of step functions indicating their locations.
+#' Their outputs are smashed together into a matrix.
+#' @return A list with values of Y, one matrix of step functions per variable.
+#'
+chooseDiverseY = function(X, n_quantiles = 10 ){
+  y = list()
+  for(k in seq(ncol(X))){
+    var_k_quantiles = quantile(X[,k], probs = (1:n_quantiles) / ( n_quantiles+1 ) )
+    y[[k]] = sapply( var_k_quantiles, function(q) { X[,k] > q } )
+  }
+  return(y)
+}
+
+#' Pick P(Y|X) purposefully to reveal bad calibration.
+#'
+#' @param X Covariates from which you want to select an active set, but you are
+#' afraid your modeling assumptions might be wrong.
+#' @param kmeans_centers The diagnostic looks for situations where the variance of
+#' a given variable differs sharply across clusters. You have to manually
+#' specify how many clusters
+#'
+#' @return A list with metadata and values of Y.
+#' Each Y is generated from a univariate step function.
+#' metadata includes the true active variable,
+#' the cutoff used in the step function,
+#' and the variable likely to be found as a false positive
+#' if homoskedastic knockoffs are used.
+#'
+chooseAdversarialY = function(X, kmeans_centers ){
+  # Cluster the dataset and look for heteroskedasticity
+  clusters = stats::kmeans(X, centers = kmeans_centers)$cluster
+  variances = matrix(NA, nrow = kmeans_centers, ncol = ncol(X))
+  for(j in unique(clusters)){
+    variances[j,] = apply(X[j==clusters,], 2, var)
+  }
+  # Cause each variable in turn to appear as a false positive
+  # Strategy: find an indicator function for a low-variance
+  # region, but based on a different variable.
+  output = data.frame(active_variable = NA,
+                      likely_false_hit = seq(ncol(X)),
+                      cutoff = NA)
+  y =list()
+  for(k in seq(ncol(X))){
+    low_variance_indicator = which.min(variances[,k])==clusters
+    output[k, "active_variable"] = which.max(abs(cor(X, low_variance_indicator)))
+    # Set the cutoff above or below to separate high from low variance
+    direction_of_association = sign(cor(low_variance_indicator, X[, output[k, "active_variable"]]))
+    output[k, "cutoff"] =
+      X[low_variance_indicator, output[k, "active_variable"]] %>%
+      quantile(0.5 - direction_of_association*0.4)
+    y[[k]] = X[,output[k, "active_variable"]] > output[k, "cutoff"]
+  }
+  return(list(metadata = output, y = y))
+}
+
 #' Given X, simulate Y|X and check calibration.
 #'
 #' @export
@@ -9,39 +69,113 @@
 #' @param groups Groups for composite hypothesis testing.
 #' @param active_set_size how many grups to include in the active set for each simulation.
 #' @param n_sim How many simulations to perform.
-#' @param FUN How to simulate Y given some x's, e.g. function(x) all(x>0) + rnorm(1)
+#' @param FUN How to simulate Y given some x's, e.g. function(x) all(x>0) + rnorm(1).
+#' Pass the string "adversarial" or "diverse" for custom options designed to reveal
+#' hidden harms of heteroskedasticity and other unexpected pitfalls.
+#' @param kmeans_centers passed to chooseAdversarialY
 #' @param plot_savepath Passed to check.calibration
+#'
 #' @importFrom magrittr %>%
 #'
 simulateY = function(X,
                      knockoffs,
                      statistic = marginalScreen,
                      groups = seq(ncol(X)),
-                     active_set_size = 5,
+                     active_set_size = 1,
                      n_sim = 500,
-                     FUN = function(x) mean(x) + 0.1*rnorm(1),
+                     FUN = function(x) x>0,
                      plot_savepath = NULL,
+                     shuddup = F,
+                     kmeans_centers = kmeans_centers,
                      ... ){
+  # You can pass in just one set, or a bunch of knockoff realizations.
   if(!is.list(knockoffs)){
     knockoffs = list(knockoffs)
   }
   stopifnot("Knockoffs must be a matrix or a list of matrices equal in size to X" = dim(knockoffs[[1                ]])==dim(X))
   stopifnot("Knockoffs must be a matrix or a list of matrices equal in size to X" = dim(knockoffs[[length(knockoffs)]])==dim(X))
-  active_group_idx    =
-    replicate(n = n_sim,
-              simplify = F,
-              sample(seq_along(groups), replace = F, size = active_set_size)
-    )
-  active_variable_idx = active_group_idx %>% lapply(function(g) Reduce(union, groups[g]) )
-  y = lapply(active_variable_idx, function(s) apply(X[,s, drop = F], 1, FUN))
+  if(length(knockoffs)<n_sim & !shuddup){
+    warning("More simulations than knockoffs. Knockoffs will be recycled.\n")
+  }
+  # You can specify Y|X and choose X's randomly, or have it done
+  # in a way designed to increase sensitivity.
+  {
+    if( identical( FUN, "adversarial" ) )
+    {
+      if( !missing( active_set_size ) ){
+        warning("Adversarial Y|X is only implemented for active_set_size=1 for now.\n")
+        active_set_size = 1
+      }
+      if( !missing( groups ) ){
+        warning("Adversarial Y|X is only implemented for non-grouped tests for now.\n")
+        # groups = seq(length.out = n_sim)
+      }
+      adversarial = chooseAdversarialY(X, kmeans_centers = kmeans_centers)
+      y = adversarial$y
+      active_group_idx = active_variable_idx = adversarial$metadata[,"active_variable"]
+      if(n_sim > length(y)){
+        warning("Adversarial y diagnostic code only generates one y per input variable. Some will be recycled.\n")
+        active_group_idx %<>% rep(length.out = n_sim) %>% as.list()
+      }
+    }
+    else if( identical( FUN, "diverse" ) )
+    {
+      if( !missing( active_set_size ) ){
+        warning("The 'diverse' scheme for Y|X is only implemented for active_set_size=1 for now.\n")
+      }
+      if( !missing( groups ) ){
+        warning("The 'diverse' scheme for Y|X is only implemented for non-grouped tests for now.\n")
+      }
+      if( !missing( statistic ) ){
+        warning("The 'diverse' scheme for Y|X has multivariate 'y' so 'statistic' will be overridden by a special default.\n")
+      }
+      active_set_size = 1
+      # groups = seq(length.out = n_sim)
+      statistic = stat.CCA
+      y = chooseDiverseY(X)
+      active_group_idx = active_variable_idx = seq_along(y)
+      if(n_sim != length(y)){
+        warning("The 'diverse' scheme for Y|X generates one y per input variable. If n_sim is different, some Y's will be recycled or ignored.\n")
+        active_group_idx %<>% rep(length.out = n_sim) %>% as.list()
+      }
+    }
+    else
+    {
+      active_group_idx    =
+        replicate(n = n_sim,
+                  simplify = F,
+                  sample(seq_along(groups), replace = F, size = active_set_size)
+        )
+      active_variable_idx = active_group_idx %>% lapply(function(g) Reduce(union, groups[g]) )
+      y = lapply(active_variable_idx, function(s) apply(X[,s, drop = F], 1, FUN))
+    }
+  }
+  # Run the knockoff filter using different knockoffs and y's each time.
+  # Recycle if needed. This recycling works best if length(y) and length(knockoffs) are
+  # relatively prime... sorryyyyyy...
+  do_one = function( i ) {
+    statistic( X = X,
+               X_k = knockoffs[[1 + magrittr::mod(i-1, length(knockoffs))]],
+               y = y[[1 + magrittr::mod(i-1, length(y))]], ... )
+  }
   W =
-    sapply( seq(n_sim), function( i ) statistic( X = X, X_k = knockoffs[[1 + magrittr::mod(i, length(knockoffs))]], y = y[[i]], ... ) ) %>%
+    sapply( seq(n_sim), do_one) %>%
     t %>%
     aggregateStats(groups)
+  # How did it do?
   calibration_grouped = checkCalibration(ground_truth = active_group_idx,
                                          W = W,
                                          plot_savepath = plot_savepath)
-  return(list(groups = groups, ground_truth = active_group_idx, stats = W, calibration = calibration_grouped))
+  return(list(
+    groups = groups,
+    ground_truth = active_group_idx,
+    stats = W,
+    calibration = calibration_grouped,
+    statistic = statistic,
+    FUN = FUN,
+    active_set_size = active_set_size,
+    n_sim = n_sim
+  ))
 }
 
 #' Given a set of knockoff stats and ground-truth nonzero entries, compute
@@ -117,3 +251,14 @@ safeCorrelation = function(...) {r = cor(...); r[is.na(r)] = 0; return(r)}
 #'
 marginalScreen =   function(X, X_k, y) as.vector(safeCorrelation(X, y) - safeCorrelation(X_k, y))
 
+#' A knockoff statistic capable of handling multivariate Y.
+#'
+#' For a single variable or knockoff x, the scheme is to regress x (as the target)
+#' on Y (as the covariates) and compute cor(x, hat x) where hat x are the fitted values.
+stat.CCA = function(X, X_k, y){
+  hat_matrix = solve(t(y) %*% y) %*% t(y)
+  do_one = function(x){
+    cor( y %*% ( hat_matrix %*% x ), x )
+  }
+  apply(X, 2, do_one) - apply(X_k, 2, do_one)
+}
--- tests/testthat/test-calibrate.R
+++ tests/testthat/test-calibrate.R
@@ -12,8 +12,47 @@ test_that("q-values match reference", {
 
 test_that("Simulations run", {
   expect_silent({
-    calibration = simulateY(X = matrix(rnorm(1000), ncol = 10),
-                            knockoffs = matrix(rnorm(1000), ncol = 10))
+    calibration = simulateY(X = matrix(rnorm(10000), ncol = 50),
+                            knockoffs = replicate(50, matrix(rnorm(10000), ncol = 50), simplify = F),
+                            n_sim = 50,
+                            shuddup = T)
   })
 
+})
+
+test_that("Diagnostic catches heteroskedasticity", {
+  # Generate heteroskedastic data
+  X = rbind(
+    matrix(rnorm(1e5), ncol = 1e2),
+    matrix(rnorm(1e5), ncol = 1e2)*0.1 + 5
+  )
+  for(k in seq(ncol(X))){
+    X[,k] = X[,k] - mean(X[,k])
+    X[,k] = X[,k] / sd(X[,k])
+  }
+  knockoffs = rlookc::computeGaussianKnockoffs(X, mu = 0, Sigma = cor(X), num_realizations = 19)
+  calibration_regular = rlookc::simulateY(X, knockoffs, FUN = function(x) x)
+  calibration_nayusty = rlookc::simulateY(X, knockoffs, FUN = "adversarial", kmeans_centers = 2)
+  calibration_diverse = rlookc::simulateY(X, knockoffs, FUN = "diverse")
+  expect_lt(
+    calibration_regular$calibration$fdr %>% colMeans %>% sum,
+    calibration_nayusty$calibration$fdr %>% colMeans %>% sum
+  )
+  expect_lt(
+    calibration_regular$calibration$fdr %>% colMeans %>% sum,
+    calibration_diverse$calibration$fdr %>% colMeans %>% sum
+  )
+  data.frame(
+    targeted_fdr = calibration_regular$calibration$targeted_fdrs,
+    random = calibration_regular$calibration$fdr %>% colMeans,
+    adversarial = calibration_nayusty$calibration$fdr %>% colMeans,
+    diverse = calibration_diverse$calibration$fdr %>% colMeans
+  ) %>%
+    tidyr::pivot_longer(cols = !targeted_fdr,
+                        names_to = "Scheme",
+                        values_to = "empirical_fdr") %>%
+    ggplot() +
+    geom_point(aes(x = targeted_fdr, y = empirical_fdr, colour = Scheme, shape = Scheme)) +
+    geom_abline(aes(intercept=0, slope=1)) +
+    ggtitle("Towards a more sensitive diagnostic")
 })
