63ed6ac85b71cb487f8af3b3362108d8b0a31fbc
ekernf01 <eric.kern13@gmail.com>
2021-09-03 12:57:57 -0400

    Small improvements to high dimensional

--- R/high_dimensional.R
+++ R/high_dimensional.R
@@ -6,13 +6,23 @@
 #' @param lambda degree of shrinkage: 0 returns the sample correlations and 1
 #' @param output_type What to return: either knockoffs themselves or parameters
 #' for downstream use in e.g. unit tests or leave-one-outs.
+#' @param silent Do not issue warnings.
 
 #' returns correlations of all 0.
 #' You can set this for optimal MSE using corpcor::estimate_lambda.
 #' @export
 #'
-highDimensionalKnockoffs = function(X, rho = 0.9, lambda = 0.1, output_type = c("knockoffs", "parameters") ){
+createHighDimensionalKnockoffs = function(X, rho = 0.9, lambda = NULL, output_type = c("knockoffs", "parameters"), seed = NULL ){
+  if(!is.null(seed)){
+    set.seed(seed)
+  }
   stopifnot("Use this only when p>>n"=nrow(X)<ncol(X))
+  if(nrow(X)<=3){
+    if(!silent){
+      warning("Too few observations to estimate mean and covariance. Returning data verbatim.")
+    }
+    return(X)
+  }
   # center to zero mean and scale to unit variance (it goes back at the end of the function)
   mu = apply(X, 2, mean)
   X = sweep(X, 2, mu, FUN = "-")
@@ -20,6 +30,10 @@ highDimensionalKnockoffs = function(X, rho = 0.9, lambda = 0.1, output_type = c(
   X = sweep(X, 2, standard_deviations, FUN = "/")
   n = nrow(X)
   svdX = svd(X, nv = nrow(X), nu = 0)
+  # Optimal shrinkage
+  if(is.null(lambda)){
+    lambda = corpcor::estimate.lambda(X, verbose = F)
+  }
   # basic_transform goes singular values to eigs of Sigma
   basic_transform = function(ti) { (1 - lambda)*ti^2/(n-1)  + lambda }
   # additional_transform goes from eigs of Sigma to user defined. Use
@@ -46,7 +60,7 @@ highDimensionalKnockoffs = function(X, rho = 0.9, lambda = 0.1, output_type = c(
                                       additional_transform = transform_eigenvectors_from_sigma_to_sqrt_c)
     # Restore previous SD
     knockoff_random = sweep(knockoff_random, 2, standard_deviations, FUN = "*")
-    return(knockoff_mean + knockoff_random)
+    return(as.matrix(knockoff_mean + knockoff_random))
   } else if(output_type == "parameters"){
     return( list(
       knockoff_mean=knockoff_mean,
